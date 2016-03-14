424be9488ddf0d6f7dd96741c47f4fb4a8706c3e
priyakhokher <pnk230@nyu.edu>
2016-03-14 02:55:55 -0700

    With added stories

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+#from .models import Post
+from django.shortcuts import render
 
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
