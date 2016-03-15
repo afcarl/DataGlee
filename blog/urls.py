7359951dd963e96e4d53c1ef6478b657bcd2bc3d
priyakhokher <pnk230@nyu.edu>
2016-03-15 02:38:13 -0700

    Yelp Story

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 url(r'^$', views.homepage, name= 'homepage'),
 url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+url(r'^yelping$', views.yelping, name='yelping'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,6 @@ def homepage(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def yelping(request):
+	return render(request, 'blog/yelping.html')
