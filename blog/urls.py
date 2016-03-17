e2fad338437a16c087cafe8e48277ea73b02c176
priyakhokher <pnk230@nyu.edu>
2016-03-17 14:16:15 -0700

    Gapminder

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ url(r'^$', views.homepage, name= 'homepage'),
 url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 url(r'^yelping$', views.yelping, name='yelping'),
 url(r'^IndianRailways$', views.IndianRailways, name='IndianRailways'),
+url(r'^Gapminder$', views.Gapminder, name='Gapminder'),
+
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from django.db import connections
+from django.db.models import Count
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+
 
 def homepage(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -16,3 +23,8 @@ def yelping(request):
 def IndianRailways(request):
 	return render(request, 'blog/IndianRailways.html')
 
+def Gapminder(request):
+	return render(request, 'blog/Gapminder.html')	
+
+# def gapminder(request):
+# 	return render(render, 'blog/gapminder.html')
