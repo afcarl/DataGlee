b3da183013922814cefeaf6ed5d2d1feaf7581a0
priyakhokher <pnk230@nyu.edu>
2016-03-19 18:45:38 -0700

    Youtube-ing_Tab error

--- blog/views.py
+++ blog/views.py
@@ -40,9 +40,8 @@ def Youtube(request):
     if request.method == 'POST':
     	a = request.POST.get("title", "")
     	s = trial.start(a)
-        a = s[0]
-        
-    	post1 = playlist.playlist_id()
+    	a = s[0]
+        post1 = playlist.playlist_id()
         #Do your stuff ,calling whatever you want from set_gpio.py
 
     	return render(request, 'blog/Youtube.html',{'post1':post1,'s':s[1:],'a':a})		
